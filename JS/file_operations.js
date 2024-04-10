2b2f722e4d11d1572b7ede8c5fd75328c2b38cde
jayamurugan <jayam.myskills@gmail.com>
2024-04-10 18:08:14 +0530

    Zerodha Landing Page

--- JS/file_operations.js
+++ JS/file_operations.js
@@ -1,4 +1,39 @@
 const fs = require("fs");
+const http = require("http");
 
 const hello = "hello world";
-console.log("hello:", hello);
+//console.log("hello:", hello);
+
+const textIn = fs.readFileSync("./input.txt", "utf-8");
+
+//console.log("textIn:", textIn);
+
+const textOut = `${textIn} additional text`;
+
+fs.writeFileSync("output.txt", textOut);
+
+console.log("File Written");
+
+const readIn = fs.readFile("./input.txt", "utf-8", (err, data) => {
+  //console.log("data:", data);
+});
+
+console.log("file read asynchroniously");
+
+const server = http.createServer((req, res) => {
+  console.log("request url:", req.url);
+  const pathName = req.url;
+  if (pathName === "/overview") {
+    res.end("This is OVERVIEW");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("Page not found");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
--- Tailwindcss/my-project/app/page.tsx
+++ Tailwindcss/my-project/app/page.tsx
@@ -9,8 +9,49 @@ export default function Home() {
       <div className="w-16 h-16 bg-black text-white underline decoration-red-500">
         Tailwindcss
       </div>
+      <p className="text-xs p-5 bg-gray-900 text-gray-500 m-3">
+        {" "}
+        just some text
+      </p>
+      <p className="tracking-wider">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, et
+        quia ducimus rerum exercitationem odio minus. Tempore, quisquam
+        exercitationem soluta obcaecati esse nulla id. Hic eos provident
+        reiciendis repudiandae voluptatum!
+      </p>
+      <p className="tracking-tight font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, et
+        quia ducimus rerum exercitationem odio minus. Tempore, quisquam
+        exercitationem soluta obcaecati esse nulla id. Hic eos provident
+        reiciendis repudiandae voluptatum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, et
+        quia ducimus rerum exercitationem odio minus. Tempore, quisquam
+        exercitationem soluta obcaecati esse nulla id. Hic eos provident
+        reiciendis repudiandae voluptatum!
+      </p>
+      <p> just some other text</p>
+      <div className="bg-red-100 m-16 w-32 h-32 border-t-8 border-blue-800 rounded-full"></div>
+      <div className="bg-red-100 m-16 w-32 h-32 border-t-8 border-blue-800 rounded-br-full"></div>
+
+      <div className="flex">
+        <div className="w-32 h-32 bg-gray-300">1</div>
+        <div className="w-32 h-32 bg-gray-300">2</div>
+      </div>
+
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Submit
+      </button>
+
+      <input
+        className="bg-white focus:bg-blue-200 border border-gray-300 rounded-lg py-2 px-4 block w-full"
+        type="email"
+        placeholder="sam@example.com"
+      ></input>
+      <div className="bg-yellow-300 w-32 h-6 hidden">world</div>
       <div className="mb-4"></div>
-      <div className="h-screen bg-slate-500">full</div>
+      <div className="font-sans h-screen bg-slate-500">full</div>
     </>
   );
 }
